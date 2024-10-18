3ca2c5474abc6f7b0253df76e63f2cd86e112a7c
czyzowicz <czyzowicz.m@gmail.com>
2024-10-18 11:30:36 +0200

    assignment

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const mainRoutes = require('./routes');
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+
+app.listen(3000);
